dc460c3143e9df96049d295734c8a129aa171c56
yanoharuto <8bitharuto@gmail.com>
2022-07-22 14:13:49 +0900

    回転する

--- CarryBattler/Assets/RollerBelt/BeltConveyor.cs
+++ CarryBattler/Assets/RollerBelt/BeltConveyor.cs
@@ -1,8 +1,34 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ベルトコンベアのベルト
+/// </summary>
 public class BeltConveyor : MonoBehaviour
 {
+    private List<GameObject> Belt = new List<GameObject>();
+    [Header("ベルトが回転するスピード")] [SerializeField] private float RotationalSpeed;
+     [Header("ベルトの右端の位置")] [SerializeField] private float BeltConveyorRightEdge;
+     [Header("ベルトの左端の位置")] [SerializeField] private float BeltConveyorLeftEdge;
+    private void Start()
+    {
+        for(int i=0;i<this.transform.childCount;i++)
+        {
+            Belt.Add(this.transform.GetChild(i).gameObject);
+        }
+    }
 
+    private void Update()
+    {
+        foreach(GameObject belt in Belt)
+        {
+            belt.transform.position += new Vector3(RotationalSpeed, 0, 0);
+
+            if (belt.transform.position.x > BeltConveyorRightEdge) 
+            {
+                belt.transform.position += new Vector3(BeltConveyorLeftEdge,0,0);
+            }
+        }
+    }
 }
