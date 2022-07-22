7e7f85ce57bdcaa9dbb5c8bcade03869e2ce97a7
yanoharuto <8bitharuto@gmail.com>
2022-07-22 14:37:45 +0900

    無限に移動するようになった

--- CarryBattler/Assets/RollerBelt/BeltConveyor.cs
+++ CarryBattler/Assets/RollerBelt/BeltConveyor.cs
@@ -9,8 +9,8 @@ public class BeltConveyor : MonoBehaviour
 {
     private List<GameObject> Belt = new List<GameObject>();
     [Header("ベルトが回転するスピード")] [SerializeField] private float RotationalSpeed;
-     [Header("ベルトの右端の位置")] [SerializeField] private float BeltConveyorRightEdge;
-     [Header("ベルトの左端の位置")] [SerializeField] private float BeltConveyorLeftEdge;
+    [Header("ベルトの右端の位置")] [SerializeField] private Vector3 BeltConveyorRightEdge;
+    [Header("ベルトの左端の位置")] [SerializeField] private Vector3 BeltConveyorLeftEdge;
     private void Start()
     {
         for(int i=0;i<this.transform.childCount;i++)
@@ -24,10 +24,11 @@ public class BeltConveyor : MonoBehaviour
         foreach(GameObject belt in Belt)
         {
             belt.transform.position += new Vector3(RotationalSpeed, 0, 0);
-
-            if (belt.transform.position.x > BeltConveyorRightEdge) 
+            //Debug.Log(belt.transform.position.x);
+            if (belt.transform.position.x > BeltConveyorRightEdge.x) 
             {
-                belt.transform.position += new Vector3(BeltConveyorLeftEdge,0,0);
+                belt.transform.localPosition=BeltConveyorLeftEdge;
+                Debug.Log(belt.transform.position.x);
             }
         }
     }
