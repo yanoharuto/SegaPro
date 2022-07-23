5bee80ff2764873b12bf4695c464a62c276150f0
yanoharuto <8bitharuto@gmail.com>
2022-07-23 15:04:59 +0900

    動かすためのレバー

--- CarryBattler/Assets/RollerBelt/Belt/Script/BeltConveyor.cs
+++ CarryBattler/Assets/RollerBelt/Belt/Script/BeltConveyor.cs
@@ -11,12 +11,15 @@ public class BeltConveyor : MonoBehaviour
     [Header("ベルトが回転するスピード")] [SerializeField] private float RotationalSpeed;
     [Header("ベルトの右端の位置")] [SerializeField] private Vector3 BeltConveyorRightEdge;
     [Header("ベルトの左端の位置")] [SerializeField] private Vector3 BeltConveyorLeftEdge;
+    private BeltConveyorEnum.BeltConveyorState BCState; 
+
     private void Start()
     {
         for(int i=0;i<this.transform.childCount;i++)
         {
             Belt.Add(this.transform.GetChild(i).gameObject);
         }
+        BCState = BeltConveyorEnum.BeltConveyorState.Stop;
     }
 
     private void Update()
@@ -24,12 +27,15 @@ public class BeltConveyor : MonoBehaviour
         foreach(GameObject belt in Belt)
         {
             belt.transform.position += new Vector3(RotationalSpeed, 0, 0);
-            //Debug.Log(belt.transform.position.x);
             if (belt.transform.position.x > BeltConveyorRightEdge.x) 
             {
                 belt.transform.localPosition=BeltConveyorLeftEdge;
-                Debug.Log(belt.transform.position.x);
             }
         }
+    }
+
+    private void InfiniteDrive()
+    {
+        
     }
 }
