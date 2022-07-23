294040e107238f03ccc60592daf911a9fd8a3928
yanoharuto <8bitharuto@gmail.com>
2022-07-23 16:20:14 +0900

    レバーが傾くようになったよ

---
+++ CarryBattler/Assets/RollerBelt/BeltLever/Lever.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField][Header("レバーの最大の傾き度合い")] private float MaxAngle;
+
+    private void Start()
+    {
+        transform.Rotate(new Vector3(0.0f,0.0f,MaxAngle));
+    }
+}
