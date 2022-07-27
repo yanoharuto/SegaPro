976db70786909a53763a2980b633f8c6ac9e5b33
yanoharuto <8bitharuto@gmail.com>
2022-07-27 15:18:47 +0900

    leverが傾くようになったよ

--- CarryBattler/Assets/RollerBelt/Lever/Lever.cs
+++ CarryBattler/Assets/RollerBelt/Lever/Lever.cs
@@ -5,16 +5,42 @@ using UnityEngine.UI;
 
 public class Lever : MonoBehaviour
 {
-    [SerializeField][Header("レバーの最大の傾き度合い")] private float MaxAngle;
-    [SerializeField][Header("ユーザーが操作するために必要なUI")] private Scrollbar LeverScrollbar;
-    [SerializeField] [Header("スクロールバーの左右に向く境目")] private float BorderValue;
+    [SerializeField] [Header("レバーの棒")] private GameObject LeverStick;
+    [SerializeField] [Range(0.1f, 1.0f)] [Header("スクロールバーの左右の境目")] private float BorderValue;
+    [SerializeField] [Range(0.1f, 1.0f)] [Header("レバーの最大の傾き度合い")] private float MaxLeverAngle;
+    [SerializeField] [Header("レバーの傾く速度")] private float LeverTiltSpeed;
+    [SerializeField] [Header("スクロールバーの値でレバーが傾く")] private Scrollbar LeverScrollbar;
+    [SerializeField] [Range(0.1f,1.0f)] [Header("スクロールバーの開始時点の値")] private float FirstScrollbarValue;
+    
     private void Start()
     {
         transform.Rotate(new Vector3(0.0f,0.0f,0.0f));
+        LeverScrollbar.value = FirstScrollbarValue;
     }
 
     private void Update()
     {
-        transform.Rotate(new Vector3(0.0f, 0.0f, LeverScrollbar.value));
+        LeverRotate();
+    }
+
+    private void LeverRotate()
+    {
+        //スクロールバーの値がBorderValue分、越しているか
+        if (LeverScrollbar.value > FirstScrollbarValue + BorderValue) 
+        {
+            Debug.Log(LeverStick.transform.localRotation.z);
+            //レバーが最大角度を超していないなら
+            if (LeverStick.transform.localRotation.z > -MaxLeverAngle) 
+            {
+               LeverStick.transform.Rotate(new Vector3(0, 0, -LeverTiltSpeed));
+            }
+        }
+        else if (LeverScrollbar.value < FirstScrollbarValue - BorderValue)
+        {
+            if (LeverStick.transform.localRotation.z < MaxLeverAngle)
+            {
+               LeverStick.transform.Rotate(new Vector3(0, 0, LeverTiltSpeed));
+            }
+        }
     }
 }
