a0e632095bf91a72c0c0b86112d93a54d223cad0
yanoharuto <8bitharuto@gmail.com>
2022-08-01 16:44:47 +0900

    手直し

---
+++ CarryBattler/Assets/RollerBelt/Script/BeltConveyor.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ベルトコンベアのベルト
+/// </summary>
+public class BeltConveyor : ConveyorIF
+{   
+    private List<GameObject> Belt = new List<GameObject>();
+    [Header("歯車が回転するスピード")] [SerializeField] private float RotationalGearSpeed;
+    [Header("ベルトが回転するスピード")] [SerializeField] private float RotationalBeltSpeed;
+    [Header("ベルトの右端の位置")] [SerializeField] private Vector3 BeltConveyorRightEdge;
+    [Header("ベルトの左端の位置")] [SerializeField] private Vector3 BeltConveyorLeftEdge;
+
+    private void Start()
+    {
+        for(int i=0;i<this.transform.childCount;i++)
+        {
+            Belt.Add(this.transform.GetChild(i).gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        InfiniteDrive();
+    }
+    /// <summary>
+    /// 無限駆動
+    /// </summary>
+    private void InfiniteDrive()
+    {
+        Vector3 Speed = new Vector3();
+
+        //右か左かで進む方向を決める
+        if(BCState==BeltConveyorEnum.BeltConveyorState.Left)
+        {
+            Speed = new Vector3(-RotationalBeltSpeed, 0, 0);
+            GearControle(-RotationalGearSpeed);
+        }
+        else if(BCState==BeltConveyorEnum.BeltConveyorState.Right)
+        {
+            Speed = new Vector3(RotationalBeltSpeed, 0, 0);
+            GearControle(RotationalGearSpeed);
+        }
+        //右でも左でもないならストップ
+        else
+        {
+            GearControle(0);
+        }
+        foreach (GameObject belt in Belt)
+        {
+            belt.transform.position += Speed;
+            //端まで行ったら逆の端に移動
+            if (belt.transform.position.x < BeltConveyorLeftEdge.x &&
+                BCState == BeltConveyorEnum.BeltConveyorState.Left)
+            {
+                belt.transform.localPosition = BeltConveyorRightEdge;
+            }
+            else if (belt.transform.position.x > BeltConveyorRightEdge.x &&
+                BCState == BeltConveyorEnum.BeltConveyorState.Right)
+            {
+                belt.transform.localPosition = BeltConveyorLeftEdge;
+            }
+        }
+    }
+    //歯車の回転の向きを決める
+    private void GearControle(float _RotationalGearSpeed)
+    {
+        foreach (GameObject belt in Belt)
+        {
+            Animator animator = belt.GetComponent<Animator>();
+            animator.SetFloat(Animator.StringToHash("Speed"), _RotationalGearSpeed);
+        }
+    }
+}
---
+++ CarryBattler/Assets/RollerBelt/Script/Lever.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField] [Header("レバーの棒のRectTransform")] private RectTransform LeverStick;
+    [SerializeField] [Range(0.1f, 1.0f)] [Header("スクロールバーの左右の境目")] private float BorderValue;
+    [SerializeField] [Range(0.1f, 1.0f)] [Header("レバーの最大の傾き度合い")] private float MaxLeverAngle;
+    [SerializeField] [Header("レバーの傾く速度")] private float LeverTiltSpeed;
+    [SerializeField] [Header("スクロールバーの値でレバーが傾く")] private Scrollbar LeverScrollbar;
+    private const float FirstScrollbarValue = 0.5f;
+    [SerializeField] [Header("BeltConveyorの親クラス")] private ConveyorIF CIF;
+    private void Start()
+    {
+        transform.Rotate(new Vector3(0.0f,0.0f,0.0f));
+        LeverScrollbar.value = FirstScrollbarValue;
+    }
+
+    private void Update()
+    {
+        LeverRotate();
+    }
+
+    private void LeverRotate()
+    {
+        //スクロールバーの値がBorderValue分、越しているか
+        if (LeverScrollbar.value > FirstScrollbarValue)
+        {
+            //レバーが最大角度を超していないなら
+            if (LeverStick.localRotation.x < MaxLeverAngle)
+            {
+                LeverStick.Rotate(new Vector3(0, 0, LeverTiltSpeed));
+            }
+        }
+        else if (LeverScrollbar.value < FirstScrollbarValue)
+        {
+            if (LeverStick.localRotation.x > -MaxLeverAngle)
+            {
+                LeverStick.Rotate(new Vector3(0, 0, -LeverTiltSpeed));
+            }
+        }
+    }
+    private void ConveyorControll()
+    {
+
+    }
+}
