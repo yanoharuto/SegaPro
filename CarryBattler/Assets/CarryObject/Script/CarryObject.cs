fd3a1c817c50f89314fd587862a6403e9cc1a8a9
yanoharuto <8bitharuto@gmail.com>
2022-08-04 14:59:40 +0900

    ダメージモーション追加

--- CarryBattler/Assets/CarryObject/Script/CarryObject.cs
+++ CarryBattler/Assets/CarryObject/Script/CarryObject.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class CarryObject : ObjectBase
 {
-    protected void OnCollisionEnter(Collision collision)
-    {
-        GameObject gameObject = collision.gameObject;
-        Debug.Log("attata");
-        if(gameObject.GetComponent<ObjectBase>().ShowTagName()==ObjectEnum.ObjectKind.Ground)
-        {
-            OnTheGround(gameObject);
-        }
-    }
-
     /// <summary>
     /// ベルトコンベアの端に到達したら端の中央に乗る
     /// </summary>
-    private void OnTheGround(GameObject _Ground)
+    protected void OnTheGround(GameObject _Ground)
     {
         Vector3 NewPos = _Ground.transform.position;
         NewPos.y = this.transform.position.y;
--- CarryBattler/Assets/Hunter/Script/Hunter.cs
+++ CarryBattler/Assets/Hunter/Script/Hunter.cs
@@ -4,4 +4,50 @@ using UnityEngine;
 
 public class Hunter: CarryObject
 {
+    [SerializeField] [Header("射程")] private float AttackRange;
+    [SerializeField] [Header("攻撃モーション")] private AnimationClip AttackAnimClip;
+    [SerializeField] [Header("特殊攻撃モーション")] private AnimationClip SPAttackAnimClip;
+    [SerializeField] [Header("待機モーション")] private AnimationClip IdolAnimClip;
+    [SerializeField] [Header("被弾時モーション")] private AnimationClip DamegedAnimClip;
+    [SerializeField] [Header("揺れ耐えモーション")] private AnimationClip ResistAnimClip;
+    private List<Animator> Animations = new List<Animator>();
+    private void Attack()
+    {
+
+    }
+    private void Dameged()
+    {
+        foreach(Animator anim in Animations)
+        {
+            anim.Play(DamegedAnimClip.name);
+        }
+    }
+    private void Idol()
+    {
+
+    }
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++) 
+        {
+            Animations.Add(this.transform.GetChild(i).GetComponent<Animator>());
+        }
+    }
+    private void Update()
+    {
+        Attack();
+        Idol();
+    }
+    protected void OnCollisionEnter(Collision collision)
+    {
+        GameObject gameObject = collision.gameObject;
+        if (gameObject.GetComponent<ObjectBase>().ShowTagName() == ObjectEnum.ObjectKind.Ground)
+        {
+            OnTheGround(gameObject);
+        }
+        if (gameObject.GetComponent<ObjectBase>().ShowTagName() == ObjectEnum.ObjectKind.Enemy)
+        {
+            Dameged();
+        }
+    }
 }
